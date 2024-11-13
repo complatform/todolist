79ab61e5ff7ce78ced93c5358731b5dc6cfaaf38
complatform <kalkidansol1623@gmail.com>
2024-11-14 00:30:31 +0300

    set to local storage change

--- src/views/Todo/TodoList.vue
+++ src/views/Todo/TodoList.vue
@@ -9,8 +9,6 @@
     <div>
       <input
         type="text"
-        name=""
-        id=""
         placeholder="add your task"
         class="shadow-lg px-7 mt-5 focus:outline-blue-300 py-3 rounded-md lg:w-96"
         v-model="taskList.tasks"
@@ -22,53 +20,32 @@
       />
 
       <button
-        class="bg-green-200 px-8 py-2 rounded-md ml-4 font-serif shadow-lg"
+        class="bg-green-200 px-8 py-2 rounded-md ml-4 font-serif shadow-lg" 
+        v-if="!taskUpdate"
         @click="addTask"
       >
-        Add Task</button
-      ><br />
+        Add Task
+      </button>
+      <button
+        class="bg-green-200 px-8 py-2 rounded-md lg:ml-4 ml-1 font-serif shadow-lg" 
+        v-if="taskUpdate"
+        @click="updateTask"
+      >
+        Update Task
+      </button><br />
       <div v-for="showTask in showTasks" :key="showTask.id">
         <div class="flex mt-10 lg:ml-36 md:ml-36 ml-2 mr-2 justify-between lg:mr-5 lg:w-2/3 md:w-2/3">
-          <p v-if="showTasks" class="font-serif lg:text-2xl md:text-xl">
-            {{ showTask.tasks }}
-          </p>
-          <p v-if="showTasks" class="font-serif lg:text-2xl ml-3 md:text-xl">
-            {{ showTask.date }}
-          </p>
-          <button
+          <p class="font-serif lg:text-2xl md:text-xl">{{ showTask.tasks }}</p>
+          <p class="font-serif lg:text-2xl ml-3 md:text-xl">{{ showTask.date }}</p>
+             <button
             class="shadow-lg lg:text-2xl px-3 py-1 rounded-md text-green-500"
             v-if="showTask.isComplated"
           >
             <i class="fa-solid fa-check lg:text-3xl md:text-xl"></i>
           </button>
-
-          <!-- <button
-            class=" bg-red-600 px-7 py-2 rounded-md text-white ml-2" v-if="!showTask.isComplated"
-            @click="deleteTask(showTask.id)"
-          >
-            Delete
-          </button> -->
-          <i
-            class="fa-regular fa-trash-can lg:text-3xl md:text-xl px-7 py-2 rounded-md text-red-400 ml-2 shadow-lg"
-            @click="deleteTask(showTask.id)"
-            v-if="!showTask.isComplated"
-          ></i>
-          <!-- <button class=" bg-blue-300 px-8 py-2 rounded-md text-white ml-2" @click="complateTask(showTask.id)" v-if="!showTask.isComplated">
-            edit
-          </button> -->
-          <!-- <i
-            class="fa-solid fa-pen-to-square lg:text-3xl md:text-xl px-7 py-2 rounded-md text-blue-300 ml-2 shadow-lg"
-            v-if="!showTask.isComplated"
-            @click="editTask(showTask.id)"
-          ></i> -->
-          <!-- <button class=" bg-green-700 px-5 py-2 rounded-md text-white ml-2" @click="complateTask(showTask.id)" v-if="!showTask.isComplated">
-            Complate
-          </button> -->
-          <i
-            class="fa-solid fa-xmark lg:text-3xl px-7 py-2 rounded-md text-green-700 ml-2 shadow-lg"
-            @click="complateTask(showTask.id)"
-            v-if="!showTask.isComplated"
-          ></i>
+          <i class="fa-regular fa-trash-can lg:text-3xl md:text-xl px-7 py-2 rounded-md text-red-400 ml-2 shadow-lg" @click="deleteTask(showTask.id)" v-if="!showTask.isComplated"></i>
+          <i class="fa-solid fa-pen-to-square lg:text-3xl md:text-xl px-7 py-2 rounded-md text-blue-300 ml-2 shadow-lg" @click="editTask(showTask.id)" v-if="!showTask.isComplated"></i>
+          <i class="fa-solid fa-xmark lg:text-3xl px-7 py-2 rounded-md text-green-700 ml-2 shadow-lg" @click="complateTask(showTask.id)" v-if="!showTask.isComplated"></i>
         </div>
       </div>
     </div>
@@ -81,42 +58,69 @@ export default {
     return {
       taskList: {
         tasks: "",
-        date: new Date(),
+        date: "",
         isComplated: false,
       },
       taskID: 1,
       showTasks: [],
+      taskUpdate: false,
     };
+  },
+  mounted() {
+    const Item = localStorage.getItem('task');
+    const ItemParse = JSON.parse(Item);
+    if (ItemParse) {
+      this.showTasks.push(...ItemParse);
+      this.taskID = this.showTasks.length ? Math.max(...this.showTasks.map(task => task.id)) + 1 : 1; 
+    }
   },
   methods: {
     addTask() {
-      this.showTasks.push({ ...this.taskList, id: this.taskID++ });
-      console.log(this.showTasks);
-      (this.taskList.tasks = ""), (this.taskList.date = null);
+      if (!this.taskList.tasks || !this.taskList.date) return; 
+      const newTask = { ...this.taskList, id: this.taskID++ };
+      this.showTasks.push(newTask);
+      this.saveTasks(); 
+      this.resetTaskList();
     },
     deleteTask(id) {
-      console.log(id);
-      this.showTasks = this.showTasks.filter((show) => show.id !== id);
+      this.showTasks = this.showTasks.filter(show => show.id !== id);
+      this.saveTasks();
     },
     complateTask(id) {
-      this.showTasks.filter((task) => {
-        if (task.id == id) {
-          task.isComplated = true;
-          console.log(task.isComplated);
-        }
-      });
+      const task = this.showTasks.find(task => task.id === id);
+      if (task) {
+        task.isComplated = true; 
+        this.saveTasks(); 
+      }
     },
-    editTask(id){
-this.showTasks.filter((task)=>{
-  if(task.id == id){
-    task.tasks=this.taskList.tasks,
-    task.date=null
-  }
-})
+    editTask(id) {
+      const taskToEdit = this.showTasks.find(task => task.id === id);
+      if (taskToEdit) {
+        this.taskList.tasks = taskToEdit.tasks;
+        this.taskList.date = taskToEdit.date;
+        this.taskID = id; 
+        this.taskUpdate = true;
+      }
+    },
+    updateTask() {
+      if (this.taskID) {
+        const taskIndex = this.showTasks.findIndex(task => task.id === this.taskID);
+        if (taskIndex !== -1) {
+          this.showTasks[taskIndex] = { ...this.taskList, id: this.taskID }; 
+          this.saveTasks(); 
+          this.resetTaskList();
+        }
+      }
+    },
+    saveTasks() {
+      localStorage.setItem('task', JSON.stringify(this.showTasks));
+    },
+    resetTaskList() {
+      this.taskList.tasks = "";
+      this.taskList.date = "";
+      this.taskUpdate = false; 
     }
   },
 };
 </script>
 
-<style>
-</style>
